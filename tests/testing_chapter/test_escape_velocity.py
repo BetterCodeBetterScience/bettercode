b341e04ad4bd5d235b344588b6f82a13af314f72
Russell Poldrack <poldrack@gmail.com>
2026-07-06 07:35:36 -0700

    fix import

--- tests/testing_chapter/test_escape_velocity.py
+++ tests/testing_chapter/test_escape_velocity.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pytest
 
-from bettercode.testing.escape_velocity_v1 import escape_velocity as escape_velocity_v1
-from bettercode.testing.escape_velocity_v2 import escape_velocity
+from bettercode.escape_velocity import (
+    escape_velocity,
+    escape_velocity_safe,
+)
 
 
 def test_escape_velocity():
@@ -12,7 +14,7 @@ def test_escape_velocity():
     mass_earth = 5.972e24  # Earth mass in kg
     radius_earth = 6.371e6  # Earth radius in meters
     ev_expected = 11186.0  # Expected escape velocity for Earth in m/s
-    ev_computed = escape_velocity_v1(mass_earth, radius_earth)
+    ev_computed = escape_velocity(mass_earth, radius_earth)
     assert np.allclose(ev_expected, ev_computed), "Test failed!"
 
 
@@ -21,4 +23,4 @@ def test_escape_velocity_negative():
     Make sure the function raises ValueError for negative mass or radius.
     """
     with pytest.raises(ValueError):
-        escape_velocity(-5.972e24, 6.371e6)
+        escape_velocity_safe(-5.972e24, 6.371e6)
